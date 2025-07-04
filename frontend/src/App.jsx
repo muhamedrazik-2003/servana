43752995c3784e54214ac26a093b458f215e142a
Muhamed Razik <muhammedrazik987@gmail.com>
2025-07-04 23:00:41 +0530

    added custom fonts and custom theme colors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <h1>Servana</h1>
+      <p>Hello How are you guys</p>
     </div>
   )
 }
