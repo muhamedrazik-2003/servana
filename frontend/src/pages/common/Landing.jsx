b832d8c6bb64ff2eaba0294ee0dcf89b8b0010c3
Muhamed Razik <muhammedrazik987@gmail.com>
2025-07-09 18:35:10 +0530

    feat : added proof section on landing

--- frontend/src/pages/common/Landing.jsx
+++ frontend/src/pages/common/Landing.jsx
@@ -6,6 +6,7 @@ import CoreFeaturesForSeeker from '../../components/common/landing/CoreFeaturesF
 import Testimonial from '../../components/common/landing/Testimonial'
 import ServiceCategory from '../../components/common/landing/ServiceCategory'
 import CoreFeaturesForProviders from '../../components/common/landing/CoreFeaturesForProviders'
+import demoDash from "../../assets/images/dashDemo.png"
 
 function Landing() {
   const [isProvider, setIsProvider] = useState(false)
@@ -14,10 +15,25 @@ function Landing() {
       <Header />
       <Hero />
       <HowItWorks isProvider={isProvider} setIsProvider={setIsProvider} />
-      <CoreFeaturesForSeeker/>
-      <ServiceCategory/>
-      <CoreFeaturesForProviders/>
-      <Testimonial isProvider={isProvider} setIsProvider={setIsProvider}/>
+      <CoreFeaturesForSeeker />
+      <ServiceCategory />
+      <CoreFeaturesForProviders />
+      <Testimonial isProvider={isProvider} setIsProvider={setIsProvider} />
+      {/* proof section (screenshot) */}
+      <section className='text-center mx-0'>
+        <h2>See Servana in Action</h2>
+        <p className='mb-[72x]'>See how easy it is to navigate, book, and oversee services on Servana.</p>
+        <div className='flex overflow-x-hidden'>
+          <div className='w-[50%] translate-x-[-20%]'>
+            <img className=' rounded-3xl shadow-lg' src={demoDash} alt="placeholder demo dash" />
+            <h5 className='pt-6 text-sm'>Customer's Dashboard</h5>
+          </div>
+          <div className='w-[50%] translate-x-[20%]'>
+            <img className=' rounded-3xl shadow-lg' src={demoDash} alt="placeholder demo dash" />
+            <h5 className='pt-6 text-sm'>Provider's Dashboard</h5>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
