c8e090ddd1fb3a8f09787eb9b1da6696e5c05f46
Muhamed Razik <muhammedrazik987@gmail.com>
2025-07-10 23:11:00 +0530

    feat : Added Donditional Based toogel between login and customer register or provider register

--- frontend/src/components/common/AuthForm.jsx
+++ frontend/src/components/common/AuthForm.jsx
@@ -3,75 +3,101 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-function AuthForm({ formType }) {
+function AuthForm({ formType, registerAs, setRegisterAs }) {
     return (
         <div className="flex flex-col items-center gap-6">
-            <div className=' w-65 flex flex-col lg:flex-row items-center justify-center gap-3'>
-                <Button variant={'outline'} size={'lg'} className={'w-full h-30 border bg-teal-100'}>Register As Customer</Button>
-                <p className='font-semibold text-xl'>or</p>
-                <Button variant={'outline'} size={'lg'} className={'w-full h-30 border bg-amber-100'}>Register As Provider</Button>
-            </div>
-            {/* <form>
-                <div className="flex flex-col gap-6">
-                    <div className="flex flex-col gap-6">
-                        <div className="space-y-4">
-                            {formType === "register"
-                                && <div className="grid gap-3">
-                                    <Label htmlFor="name">Full Name</Label>
-                                    <Input
-                                        id="name"
-                                        type="name"
-                                        placeholder="Jhon Doe"
-                                        required
-                                    />
+            {
+                (formType === "register" && registerAs === null)
+                    ? <div className=' w-65 flex flex-col items-center justify-center gap-3'>
+                        <Button
+                            onClick={() => setRegisterAs("seeker")}
+                            variant={'outline'} size={'lg'}
+                            className={'w-full h-30 border bg-teal-100 shadow-xl hover:bg-secondary'}>
+                            Register As Customer
+                        </Button>
+                        <p className='font-semibold text-xl'>or</p>
+                        <Button
+                            onClick={() => setRegisterAs("provider")}
+                            variant={'outline'} size={'lg'}
+                            className={'w-full h-30 border bg-amber-100 shadow-xl'}>
+                            Register As Provider
+                        </Button>
+                    </div>
+                    : <form className=" w-65 lg:w-full">
+                        <div className="flex flex-col gap-6">
+                            <div className="flex flex-col gap-6">
+                                <div className="space-y-4">
+                                    {formType === "register"
+                                        && <div className="grid gap-3">
+                                            <Label htmlFor="name">Full Name</Label>
+                                            <Input
+                                                id="name"
+                                                type="name"
+                                                placeholder="Jhon Doe"
+                                                required
+                                            />
+                                        </div>
+                                    }
+                                    <div className="grid gap-3">
+                                        <Label htmlFor="email">Email</Label>
+                                        <Input
+                                            id="email"
+                                            type="email"
+                                            placeholder="m@example.com"
+                                            required
+                                        />
+                                    </div>
+                                    <div className="grid gap-3">
+                                        <Label htmlFor="password">Password</Label>
+                                        <Input
+                                            id="password"
+                                            type="password"
+                                            placeholder="Example@123"
+                                            required
+                                        />
+                                    </div>
+                                </div>
+                                <Button type="submit" className="w-full">
+                                    Login
+                                </Button>
+                            </div>
+                            <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                                <span className="bg-background text-muted-foreground relative z-10 px-2">
+                                    Or
+                                </span>
+                            </div>
+                            <div className="grid grid-cols-1">
+                                <Button variant="outline" type="button" className="w-full border">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <path
+                                            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                                            fill="currentColor"
+                                        />
+                                    </svg>
+                                    Continue with Google
+                                </Button>
+                            </div>
+                            {
+                                formType === "login"
+                                ? <div className="text-center text-sm">
+                                    <a href="#" className="underline underline-offset-4 text-primary">
+                                        Forgot Password ?
+                                    </a>
+                                    </div>
+                                : <div className="text-center text-sm">
+                                    <p 
+                                        onClick={() => setRegisterAs(null)}
+                                        className="underline underline-offset-4 text-primary">
+                                       ← Back to Role Selection
+                                    </p>
                                 </div>
                             }
-                            <div className="grid gap-3">
-                                <Label htmlFor="email">Email</Label>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    placeholder="m@example.com"
-                                    required
-                                />
-                            </div>
-                            <div className="grid gap-3">
-                                <Label htmlFor="password">Password</Label>
-                                <Input
-                                    id="password"
-                                    type="password"
-                                    placeholder="Example@123"
-                                    required
-                                />
-                            </div>
+
                         </div>
-                        <Button type="submit" className="w-full">
-                            Login
-                        </Button>
-                    </div>
-                    <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-                        <span className="bg-background text-muted-foreground relative z-10 px-2">
-                            Or
-                        </span>
-                    </div>
-                    <div className="grid gap-4 sm:grid-cols-2">
-                        <Button variant="outline" type="button" className="w-full border">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <path
-                                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                                    fill="currentColor"
-                                />
-                            </svg>
-                            Continue with Google
-                        </Button>
-                    </div>
-                    <div className="text-center text-sm">
-                        <a href="#" className="underline underline-offset-4 text-primary">
-                            Forgot Password ?
-                        </a>
-                    </div>
-                </div>
-            </form> */}
+                    </form>
+            }
+
+
         </div>
     )
 }
--- frontend/src/pages/common/Auth.jsx
+++ frontend/src/pages/common/Auth.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import AuthForm from '../../components/common/AuthForm'
 import { useState } from 'react'
+import AuthForm from '../../components/common/AuthForm'
 import Logo from "../../assets/images/logo.png"
 
 function Auth() {
   const [isRegistered, setIsRegistered] = useState(false)
+  const [registerAs, setRegisterAs] = useState(null)
+
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="w-full max-w-sm space-y-4">
+      <div className="w-full h-[575px] max-w-sm space-y-4">
         <div >
           <div className='flex items-center justify-center'>
             <img className='size-10' src={Logo} alt="logo image" />
@@ -15,14 +16,28 @@ function Auth() {
           </div>
           <p className="text-[14px] lg:text-lg text-center">Find trusted services around you</p>
         </div>
-        <div className={`relative shadow-sm border w-[65%] md:max-w-[50%] rounded-4xl mx-auto text-center flex items-center mb-[2.5rem] lg:mb-[2rem] transition-all duration-100 ${isRegistered ? "bg-amber-100" : "bg-teal-100"}`}>
-          <div className={`absolute left-0 rounded-3xl mx-1 w-[50%] h-8 transition-all duration-300 ${isRegistered ? 'translate-x-[93%] bg-accent' : 'bg-secondary'}`}></div>
-          <h4 onClick={() => { setIsRegistered(false); console.log(isRegistered) }} className='z-10 text-sm cursor-pointer p-2 w-[50%]' >Login</h4>
-          <h4 onClick={() => setIsRegistered(true)} className={`z-10 text-sm p-2 w-[50%] cursor-pointer ${isRegistered ? "text-white" : 'text-foreground'}`} >Register</h4>
+        <div className={`relative shadow-sm border w-[65%] lg:w-full rounded-4xl mx-auto text-center flex items-center mb-[2.5rem] lg:mb-[2rem] transition-all duration-100 ${(isRegistered && registerAs === "provider") ? "bg-amber-100" : (isRegistered && registerAs === "seeker") ? "bg-teal-100" : "bg-violet-200"}`}>
+          <div className={`absolute left-0 rounded-3xl mx-1 w-[50%] h-8 lg:h-9.5 transition-all duration-300 ${isRegistered ? 'translate-x-[93%] lg:translate-x-[96%]' : ''} ${(isRegistered && registerAs === "provider") ? "bg-accent" : (isRegistered && registerAs === "seeker") ? "bg-secondary" : 'bg-primary'}`}></div>
+          <h4
+            onClick={() => {
+              setIsRegistered(false);
+              setRegisterAs(null);
+            }}
+            className={`z-10 text-sm cursor-pointer p-2 lg:p-3 w-[50%] ${isRegistered ? "" : "text-white" }`} >
+            Login
+          </h4>
+          <h4
+            onClick={() => {
+              setIsRegistered(true);
+              setRegisterAs(null);
+            }}
+            className={`z-10 text-sm p-2 lg:p-3 w-[50%] cursor-pointer ${(isRegistered && registerAs === "seeker") ? "text-black" : (isRegistered && registerAs === "provider" || isRegistered) ? "text-white" : "text-black"}`} >
+            Register
+          </h4>
         </div>
         {isRegistered
-          ? <AuthForm formType={"register"} />
-          : <AuthForm formType={"login"} />
+          ? <AuthForm formType={"register"} registerAs={registerAs} setRegisterAs={setRegisterAs} />
+          : <AuthForm formType={"login"} registerAs={registerAs} setRegisterAs={setRegisterAs} />
         }
       </div>
     </div>
