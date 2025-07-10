be048260f10029a627a0d6ad35d1ebb8d0189031
Muhamed Razik <muhammedrazik987@gmail.com>
2025-07-10 19:19:32 +0530

    added responsiveness to about page and removed unwanted images

--- frontend/src/pages/common/About.jsx
+++ frontend/src/pages/common/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from "../../components/common/Header"
 import aboutHeroImage from "../../assets/images/AboutHero.png"
-import communityImage from "../../assets/images/Stylized Miniature Model Town.png"
+import communityImage from "../../assets/images/whyServanaImage.png"
 import Testimonial from '../../components/common/landing/Testimonial'
 import Footer from "../../components/common/Footer"
 import CallToAction from '../../components/common/landing/CallToAction'
@@ -60,16 +60,16 @@ function About() {
     <main>
       <Header />
 
-      <section className='flex gap-6 mx-[180px] pt-[100px]'>
+      <section className='flex flex-col lg:flex-row gap-6 lg:mx-[180px] pt:-[50px] lg:pt-[100px]'>
         <div>
-          <h1 className='text-[64px] leading-18 text-start'>Helping Locals Find Trusted Services, Instantly.</h1>
+          <h1 className='text-[clamp(2rem,6vw,64px)] leading-12 lg:leading-18 text-start'>Helping Locals Find Trusted Services, Instantly.</h1>
           <p className='text-lg font-semibold mb-24px lg:mb-[52px]'>At Servana, we believe that every neighborhood deserves access to trusted professionals—delivered fast, fairly, and safely.</p>
         </div>
         <img className='size-85' src={aboutHeroImage} alt="" />
       </section>
 
       <section>
-        <div className="mx-auto px-6 lg:px-12 flex flex-col-reverse lg:flex-row items-center gap-12">
+        <div className="mx-auto px-6 lg:px-12 flex flex-col lg:flex-row items-center gap-12">
           <div className="lg:w-1/2 ">
             <h2>Why We Started</h2>
             <p className="text-lg leading-relaxed">
