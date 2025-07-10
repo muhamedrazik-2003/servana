8a4d1829cfb8195a29d290b03c90f267a2fc345a
Muhamed Razik <muhammedrazik987@gmail.com>
2025-07-10 18:22:01 +0530

    feat : Added responsiveness to CallToAction

--- frontend/src/components/common/landing/CallToAction.jsx
+++ frontend/src/components/common/landing/CallToAction.jsx
@@ -5,14 +5,14 @@ import finalCtaBackground from "../../../assets/images/ctaBackground.png"
 function CallToAction() {
     return (
         <section className='text-center relative'>
-            <h2 className='text-6xl'>Ready to Get Started?</h2>
-            <p>Become part of a community that values quality and simplicity.</p>
-            <div className='flex items-center gap-x-8 justify-center mt-[100px]'>
+            <h2 className='text-[2.5rem] lg:text-6xl'>Ready to Get Started?</h2>
+            <p className='mb-20'>Become part of a community that values quality and simplicity.</p>
+            <div className='flex flex-col lg:flex-row items-center justify-center gap-x-8'>
                 <Link to={'/auth'}><Button variant={'seeker'} size={'lg'}>Find a Service</Button></Link>
                 <p className='font-semibold text-xl'>or</p>
                 <Link to={'/auth'}><Button variant={'provider'} size={'lg'}>Become a Provider</Button></Link>
             </div>
-            <img className='absolute top-[10%] left-[50%] translate-x-[-50%] w-[220px] z-[-1]' src={finalCtaBackground} alt="" />
+            <img className='absolute top-[30%] lg:top-[10%] left-[50%] translate-x-[-50%] w-[160px] lg:w-[220px] z-[-1]' src={finalCtaBackground} alt="" />
         </section>
 
     )
