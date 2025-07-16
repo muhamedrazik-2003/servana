0d361d183b85046919c30218899331a0979fb55e
Muhamed Razik <muhammedrazik987@gmail.com>
2025-07-16 16:05:05 +0530

    add already done missing components to repo

--- frontend/src/pages/seeker/MyBookings.jsx
+++ frontend/src/pages/seeker/MyBookings.jsx
@@ -10,7 +10,7 @@ function MyBookings() {
       <section className='mt-12 pb-5 md:pb-10'>
         <h1 className='text-[clamp(2.5rem,8vw,48px)] leading-11  md:leading-18 z-0 mb-[1rem]'>My Bookings</h1>
         <p className='max-w-[600px] text-sm mx-auto font-semibold text-center mb-6 md:mb-10'>View your upcoming, ongoing, and past service bookings with real-time updates..</p>
-        <div className="relative w-[340px] md:w-[600px] mx-auto">
+        <div className="relative w-[340px] md:w-[650px] mx-auto">
           <MapPin className="absolute left-3 top-3 size-5 md:size-6 text-primary" />
           <input
             type="text"
