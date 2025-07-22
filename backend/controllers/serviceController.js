ac95b32916a07c602fd27b37773d7bbbf9735dc1
Muhamed Razik <muhammedrazik987@gmail.com>
2025-07-22 20:46:04 +0530

    configured cloudinary and created .gitignore

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -1,10 +1,24 @@
 const services = require('../models/serviceModel');
 
-exports.getServices = async(req, res) => {
+exports.addService = async(req, res) => {
     try {
-        const {}
-
+        const {title, description, category, subCategory,price,priceUnit, location} = req.body;
+        const providerId = req.user;
+        const images = req.file.filename;
+        const newService = 
+        res.status(200).json({message : "services of Provider retrieved",ServiceList})
     } catch (error) {
-        res.status(500).json({message: "Internal Server Error", error})
+        res.status(404).json({message: "Failed To add Service", error})
+    }
+}
+
+exports.getUserServices = async(req, res) => {
+    try {
+        const providerId = req.user;
+        console.log(providerId)
+        const ServiceList =await services.find({providerId})
+        res.status(200).json({message : "services of Provider retrieved",ServiceList})
+    } catch (error) {
+        res.status(404).json({message: "Failed to retrieve provider services", error})
     }
 }
