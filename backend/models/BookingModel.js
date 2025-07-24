2966507addd95de5ee6822415eae5235c1b2f8a0
Muhamed Razik <muhammedrazik987@gmail.com>
2025-07-25 01:13:19 +0530

    Created BookingModel

---
+++ backend/models/BookingModel.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  serviceId: {
+    type: String,
+    required: true,
+  },
+  seekerId: {
+    type: String,
+    required: true,
+  },
+  providerId: {
+    type: String,
+    required: true,
+  },
+  scheduledDate: {
+    type: String,
+    required: true,
+  },
+  scheduledTime: {
+    type: String,
+    required: true,
+  },
+  location: {
+    city: { type: String },
+    state: { type: String },
+    pincode: { type: String },
+  },
+  seekerNotes : {
+    type:String
+  },
+  paymentStatus : {
+    type:String,
+    enum: ["paid", "pending"],
+    default: "pending",
+  },
+  bookingStatus : {
+    type:String,
+    enum: ["ongoing", "pending", "completed", "failed", "cancelled"],
+    default: "pending",
+  },
+  reason : {
+    type: String,
+  },
+  duration : {
+    type : Number,
+  },
+  durationUnit : {
+    type: String,
+    enum : ["hour", "day", "service"],
+    default : "hour"
+  }
+},
+{timestamps: true}
+);
+
+const bookings = mongoose.model("bookings", bookingSchema);
+module.exports = bookings;
